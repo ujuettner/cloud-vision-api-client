37879f9e428a0d8647158e94d1be0f7fd13a96ed
Udo Juettner <udo.juettner@gmail.com>
2021-01-13 20:20:25 +0100

    Refactor flow to separate funcs

--- main.go
+++ main.go
@@ -17,7 +17,8 @@ import (
 func main() {
 	gcsBucket := flag.StringP("bucket", "b", "", "GCS bucket")
 	imageFile := flag.StringP("image", "i", "", "image file")
-	uploadTimeoutSecs := flag.Float64P("timeout", "t", 60.0, "timeout in seconds for image upload")
+	timeout := flag.DurationP("timeout", "t", 30.0*time.Second, "timeout for all remote calls")
+	maxResults := flag.Int32P("maxresults", "m", 3, "maximum number of results per category")
 	flag.Parse()
 
 	if *gcsBucket == "" {
@@ -29,7 +30,7 @@ func main() {
 		os.Exit(2)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(*uploadTimeoutSecs))
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	defer cancel()
 
 	uri, err := uploadImage(ctx, *imageFile, *gcsBucket)
@@ -38,29 +39,13 @@ func main() {
 		return
 	}
 
-	// TODO: extract to func
-	iac, err := vision.NewImageAnnotatorClient(ctx)
-	if err != nil {
-		fmt.Printf("Error creating the image annotator client: %v\n", err)
-		return
-	}
-	defer iac.Close()
-
-	res, err := iac.AnnotateImage(ctx, &visionpb.AnnotateImageRequest{
-		Image: vision.NewImageFromURI(uri),
-		Features: []*visionpb.Feature{
-			{Type: visionpb.Feature_LANDMARK_DETECTION, MaxResults: 8},
-			{Type: visionpb.Feature_LABEL_DETECTION, MaxResults: 4},
-		},
-	})
+	res, err := getImageAnnotations(ctx, uri, *maxResults)
 	if err != nil {
 		fmt.Printf("Error getting image annotations: %v\n", err)
 		return
 	}
 
-	// TODO: format output???
-	// see https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/vision/v1#AnnotateImageResponse
-	fmt.Printf("%v\n", res)
+	printResults(res)
 }
 
 func uploadImage(ctx context.Context, imageFile, bucket string) (string, error) {
@@ -86,4 +71,46 @@ func uploadImage(ctx context.Context, imageFile, bucket string) (string, error) 
 	}
 
 	return fmt.Sprintf("gs://%s/%s", object.BucketName(), object.ObjectName()), nil
+}
+
+func getImageAnnotations(ctx context.Context, imageUri string, maxResults int32) (*visionpb.AnnotateImageResponse, error) {
+	iac, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating the image annotator client: %v\n", err)
+	}
+	defer iac.Close()
+
+	res, err := iac.AnnotateImage(ctx, &visionpb.AnnotateImageRequest{
+		Image: vision.NewImageFromURI(imageUri),
+		Features: []*visionpb.Feature{
+			{Type: visionpb.Feature_LANDMARK_DETECTION, MaxResults: maxResults},
+			{Type: visionpb.Feature_LOGO_DETECTION, MaxResults: maxResults},
+			{Type: visionpb.Feature_LABEL_DETECTION, MaxResults: maxResults},
+			{Type: visionpb.Feature_TEXT_DETECTION, MaxResults: maxResults},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Error getting image annotations: %v\n", err)
+	}
+
+	return res, nil
+}
+
+func printResults(res *visionpb.AnnotateImageResponse) {
+	fmt.Println("Landmarks:")
+	for i, v := range res.GetLandmarkAnnotations() {
+		fmt.Printf("\t%d: %s (%f)\n", i, v.Description, v.Score)
+	}
+	fmt.Println("Logos:")
+	for i, v := range res.GetLogoAnnotations() {
+		fmt.Printf("\t%d: %s (%f)\n", i, v.Description, v.Score)
+	}
+	fmt.Println("Labels:")
+	for i, v := range res.GetLabelAnnotations() {
+		fmt.Printf("\t%d: %s (%f)\n", i, v.Description, v.Score)
+	}
+	fmt.Println("Text:")
+	for i, v := range res.GetTextAnnotations() {
+		fmt.Printf("\t%d: %s\n", i, v.Description)
+	}
 }
